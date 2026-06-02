928cde12535824de4ec891a30b2125815f348ea5
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2026-06-02 15:27:51 +0500

    Created GitHub Action

--- convert_xml.py
+++ convert_xml.py
@@ -2,17 +2,20 @@ import os
 import sys
 import shutil
 import subprocess
+from pathlib import Path
 
-DUMP = r"./_dump"
+script_dir = Path(__file__).resolve().parent
+DUMP = script_dir / "_dump"
+
 # Renew DUMP
 if os.path.exists(DUMP): shutil.rmtree(DUMP)
 shutil.copytree(sys.argv.pop(), DUMP)
-if sys.argv[1] == "--internal": shutil.copytree(r"./doc/classes", DUMP, dirs_exist_ok=True)
+if sys.argv[1] == "--internal": shutil.copytree(script_dir / "doc/classes", DUMP, dirs_exist_ok=True)
 elif sys.argv[1] != "--no-internal":
     raise ValueError('Expected "--internal" or "--no-internal"')
 
 process = subprocess.Popen(
-    [sys.executable, "./src/make_rst.py",  *sys.argv[2:], DUMP],
+    [sys.executable, script_dir / "src/make_rst.py",  *sys.argv[2:], DUMP],
     stdout=subprocess.PIPE,
     stderr=subprocess.STDOUT,
     text=True
